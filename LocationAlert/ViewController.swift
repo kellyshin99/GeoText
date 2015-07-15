9c629b7639c952d117fe72b1f62f95bf610ae922
Kelly Seongeun Shin <kellysshin99@gmail.com>
2015-07-15 10:21:36 -0700

    Initial Commit

---
+++ LocationAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationAlert
+//
+//  Created by Kelly Shin on 7/15/15.
+//  Copyright (c) 2015 KellyShin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
